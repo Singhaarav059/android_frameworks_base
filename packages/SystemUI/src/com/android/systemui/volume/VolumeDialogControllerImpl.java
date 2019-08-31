fa75a258ab48240e8c9539a0d13faf1220525781
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-31 02:10:39 -0300

    VolumeDialogControllerImpl: Check for internal audio recording support
    Change-Id: Ia37b3f8620fbe5daa7175bcd208620447dbfedb8
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -74,6 +74,8 @@ import java.util.Objects;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.android.internal.util.custom.recorder.InternalAudioRecorder;
+
 /**
  *  Source of truth for all state / events related to the volume dialog.  No presentation.
  *
@@ -138,6 +140,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
     private UserActivityListener mUserActivityListener;
 
     protected final VC mVolumeController = new VC();
+    private boolean mIsInternalAudioRecordingSupported;
 
     private boolean mIsInternalAudioRecordingSupported;
 
