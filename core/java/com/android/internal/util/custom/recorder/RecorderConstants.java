PixelExperience/frameworks_base

SystemUI: Whitelist our recorder on CastController
Change-Id: Ifeec0d416d0ae30e01fbf659c6a48e1c650cf284

core/java/com/android/internal/util/custom/recorder/RecorderConstants.java
@@ -15,10 +15,6 @@
 */
 package com.android.internal.util.custom.recorder;
 
-import android.content.Context;
-
-public class InternalAudioRecorder {
-    public static boolean isSupported(Context context) {
-        return context.getResources().getBoolean(com.android.internal.R.bool.config_hasInternalAudioRecordingSupport);
-    }
+public class RecorderConstants {
+    public static final String APP_PACKAGE_NAME = "org.pixelexperience.recorder";
 }

Singhaarav059/android_frameworks_base

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

core/java/com/android/internal/util/custom/recorder/RecorderConstants.java
@@ -15,10 +15,6 @@
 */
 package com.android.internal.util.custom.recorder;
 
-import android.content.Context;
-
-public class InternalAudioRecorder {
-    public static boolean isSupported(Context context) {
-        return context.getResources().getBoolean(com.android.internal.R.bool.config_hasInternalAudioRecordingSupport);
-    }
+public class RecorderConstants {
+    public static final String APP_PACKAGE_NAME = "org.lineageos.recorder";
 }